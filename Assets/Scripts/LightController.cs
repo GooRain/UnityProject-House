3a4a3457e2959141ccc9a43a1c67baf735a212ef
kharitonoff1193 <kharitonoff1193l@gmail.com>
2017-04-25 18:13:17 +0900

    -Lerp lighting

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -2,10 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightController : MonoBehaviour {
+public class LightController : MonoBehaviour
+{
 
 	public Light lamp;
-	public void Toggle () {
+	public GameObject floor;
+	Color newColor = new Color(0, 0, 0);
+	public float rate = 2f;
+	float counter = 0f;
+	bool isChangingColor = false;
+	float t = 0;
+	float r, g, b;
+
+	private void Update()
+	{
+		//r += (int)(100 * Time.deltaTime);
+		//g += (int)(100 * Time.deltaTime);
+		//b += (int)(100 * Time.deltaTime);
+		//Debug.Log(r);
+
+		counter -= Time.deltaTime;
+
+		if (counter <= 0 && !isChangingColor)
+		{
+			newColor = Random.ColorHSV();
+			isChangingColor = true;
+		}
+
+		lamp.color = LerpRGB(lamp.color, newColor, t);
+
+		if (t < 1)
+		{
+			t += Time.deltaTime / rate;
+			//t = Mathf.Clamp(t, 0, 1);
+		}
+		else
+		{
+			counter = 0.1f;
+			isChangingColor = false;
+			t = 0;
+		}
+	}
+	public void Toggle()
+	{
 		lamp.enabled = !lamp.enabled;
+		// if (floorSwitch)
+		//   floor.GetComponent<Renderer>().material.color = Color.white;
+		//else
+		//    floor.GetComponent<Renderer>().material.color = Color.rg;
+
+		floor.GetComponent<Renderer>().material.color = Random.ColorHSV();
+		//floorSwitch = !floorSwitch;
+	}
+
+	public Color LerpRGB(Color a, Color b, float t)
+	{
+		return new Color(
+		a.r + (b.r - a.r) * t,
+		a.g + (b.g - a.g) * t,
+		a.b + (b.b - a.b) * t,
+		a.a + (b.a - a.a) * t
+	);
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,64 +1,73 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float mouseSensitivity = 250f;
-	public float speed = 15f;
-	public float interactionDistance = 3f;
-	RaycastHit rcHit;
+    public float mouseSensitivity = 250f;
+    public float speed = 15f;
+    public float interactionDistance = 3f;
+    RaycastHit rcHit;
 
-	Transform camT;
-	float rotY;
-	Rigidbody rb;
+    Transform camT;
+    float rotY;
+    Rigidbody rb;
 
-	void Start() {
-		Cursor.visible = false;
-		camT = Camera.main.transform;
-		rb = GetComponent<Rigidbody>();
-	}
+    void Start()
+    {
+        Cursor.visible = false;
+        camT = Camera.main.transform;
+        rb = GetComponent<Rigidbody>();
+    }
 
-	void Update() {
+    void Update()
+    {
 
-		//	Movement
-		float movementX = Input.GetAxisRaw("Horizontal");
-		float movementZ = Input.GetAxisRaw("Vertical");
+        //	Movement
+        float movementX = Input.GetAxisRaw("Horizontal");
+        float movementZ = Input.GetAxisRaw("Vertical");
 
-		Vector3 moveX = transform.right * movementX;
-		Vector3 moveZ = transform.forward * movementZ;
+        Vector3 moveX = transform.right * movementX;
+        Vector3 moveZ = transform.forward * movementZ;
 
-		Vector3 movement = (moveX + moveZ).normalized * speed;
+        Vector3 movement = (moveX + moveZ).normalized * speed;
 
-		if(movement != Vector3.zero) {
-			//Debug.Log("moving");
-			rb.MovePosition(rb.position + movement * Time.deltaTime);
-		}
+        if (movement != Vector3.zero)
+        {
+            //Debug.Log("moving");
+            rb.MovePosition(rb.position + movement * Time.deltaTime);
+        }
 
-		//	Mouse rotation
-		transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity);
-		rotY += Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
-		rotY = Mathf.Clamp(rotY, -80, 80);
-		camT.localEulerAngles = Vector3.left * rotY;
+        //	Mouse rotation
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity);
+        rotY += Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        rotY = Mathf.Clamp(rotY, -80, 80);
+        camT.localEulerAngles = Vector3.left * rotY;
 
-		//	Interaction ray
-		Vector3 eyePosition = camT.position;
-		Debug.DrawRay(eyePosition, camT.transform.forward, Color.red);
-		if(Physics.Raycast(eyePosition, camT.transform.forward, out rcHit, interactionDistance)) {
-			if(Input.GetButtonDown("Fire1")) {
-				Debug.Log("Interact");
-				if(rcHit.collider.gameObject.GetComponentInParent<AnimController>()) {
-					Debug.Log("Interactable object");
-					rcHit.collider.gameObject.GetComponentInParent<AnimController>().Animate();
-				}
-				if(rcHit.collider.gameObject.GetComponentInParent<LightController>()) {
-					Debug.Log("Interactable object");
-					rcHit.collider.gameObject.GetComponentInParent<LightController>().Toggle();
-				}
-			}
-		}
+        //	Interaction ray
+        Vector3 eyePosition = camT.position;
+        Debug.DrawRay(eyePosition, camT.transform.forward, Color.red);
+        if (Physics.Raycast(eyePosition, camT.transform.forward, out rcHit, interactionDistance))
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("Interact");
+                if (rcHit.collider.gameObject.GetComponentInParent<AnimController>())
+                {
+                    Debug.Log("Interactable object");
+                    rcHit.collider.gameObject.GetComponentInParent<AnimController>().Animate();
+                }
+                if (rcHit.collider.gameObject.GetComponentInParent<LightController>())
+                {
+                    Debug.Log("Interactable object");
+                    rcHit.collider.gameObject.GetComponentInParent<LightController>().Toggle();
+                }
+            }
+        }
 
-		if(Input.GetKeyDown(KeyCode.Escape)) {
-			SceneManager.GetSceneByName("menu");
-		}
-	}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.GetSceneByName("menu");
+        }
+    }
 }
